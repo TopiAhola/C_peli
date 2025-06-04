8acce971017bbaf3015a11d74323e2500756a325
TopiAhola <topi.ahola@metropolia.fi>
2025-06-04 23:48:23 +0300

    Daily 6 completed
    -nuc

--- Dailys/6_gamepad_input.cpp
+++ Dailys/6_gamepad_input.cpp
@@ -5,6 +5,7 @@
 #include <xinput.h>
 
 
+
 /////////////////////////////////////////////////////////////
 //DEFINES                      
                                 // static remembers its value like a global but is still limited in scope
@@ -12,6 +13,7 @@
 #define global_static static    // for global statics
 #define internal static         //static has 3rd meaning with functions limiting use of their name?
  
+
 
 /////////////////////////////////////////////////////////////
 //TESTING LOADING WINDOWS FUNCTIONS MANUALLY
@@ -40,11 +42,20 @@ global_static x_input_set_state * p_XInputSetState_stub = XInputSetState_stub;
 #define XInputGetState p_XInputGetState_stub
 #define XInputSetState p_XInputSetState_stub
 
-//Loads XInput library
-internal void load_xinput(){ 
-    
+//Loads XInput library, sets function calls to point at real Windows functions instead of stubs
+//Win 11 doesn't have this library? Maybe put a copy in program folder, LoadLibrary does search there first?
+//Win 11 has Xinput9_1_0.dll which has the functions?
+internal void load_xinput(void){ 
+    HMODULE XInputLibrary = LoadLibraryA("Xinput9_1_0.dll");
+    OutputDebugStringA("load_xinput function\n");
+    if(XInputLibrary){
+        XInputGetState = (x_input_get_state *) GetProcAddress(XInputLibrary, "XInputGetState");
+        XInputSetState = (x_input_set_state *) GetProcAddress(XInputLibrary, "XInputSetState");
+        OutputDebugStringA("Xinput.dll library functions loaded manually\n");
+    }
  
 }
+
 
 
 /////////////////////////////////////////////////////////////
@@ -67,53 +78,68 @@ struct window_dimensions {
 
 
 
+
+/////////////////////////////////////////////////////////////
 //GLOBALS
 global_static bool program_running;      //this is toggled false when program needs to quit
 global_static offscreen_bitmap backbuffer = {};  //backbuffer is a offscreen_bitmap struct
 global_static offscreen_bitmap * p_backbuffer = &backbuffer;
 
+//KB input test globals TODO: remove?
+int kb_x_input;
+int kb_y_input;
+
+
 /////////////////////////////////////////////////////////////
 //FUNCTIONS
 
-
-//Returns dimensions of RECT for a given window
-window_dimensions get_window_dimensions(HWND window){
-    RECT client_rect;
-    GetClientRect(window, &client_rect);
-    window_dimensions dimensions;
-    dimensions.width = client_rect.right - client_rect.left;
-    dimensions.height = client_rect.bottom - client_rect.top;
-    return dimensions;
-};
-
-
+////////////////////////////////
 //Takes a pointer to a bitmap. Draws something to the given bitmap.  
 internal void
-draw_test(offscreen_bitmap * bitmap){
+draw_test(offscreen_bitmap * bitmap, int y_input = 0 , int x_input = 0){
     
-    OutputDebugStringA("draw test");
+    OutputDebugStringA("draw test\n");
+
+
+    //location for a green spot in bitmap
+    local_static int y_location = 0;  
+    y_location = y_location + y_input;
+    local_static int x_location = 0;  
+    x_location = x_location + x_input;
+    UINT8 green_spot;
+    UINT8 * p_green_spot = & green_spot; 
+   
 
     local_static int intensity = 0; //intensity increases every function call until it overflows    
     int pitch = bitmap->width*bitmap->bytes_per_pixel;  //lenght of a row in bitmap
         
+    
     UINT8 * p_row = (UINT8 *)bitmap->p_memory; //cast void bitmap pointer to 8 BIT INT
     for(int y=0; y<bitmap->height; y++)
-    {
+    {   
+
         UINT32 * p_pixel = (UINT32*)p_row;     //recast to 32 bit to point argb values
         for(int x=0; x<bitmap->width; x++)
-        {
+        {   
             //p_pixel = p_pixel + x;    //iterates over pixels, SEGFAULTS
 
             //WINDOWS has colors BGRx but this has xRGB but little endian!
             //*p_pixel = 0x00500000;      //argb LITTLE ENDIAN MEANS VALUES HAVE SMALLER NUMBERS FIRST
             //It writes smallest bit to first byte so the hexa is still xRGB when read with 32 bit pointer
             
-            //ANIMATED PER COLOR VERSION     
+            //Animated background with a green movable crosshair  
 
+            if (x == x_location or y == y_location){   //set lines at x and y location
+                *p_green_spot = 255;
+            } else {
+                *p_green_spot = 0;
+            }          
+
+            
             UINT8 * p_color = (UINT8 *)p_pixel;
             *p_color = intensity; //blue from argument
             p_color++;
-            *p_color = 0; //green to upper left
+            *p_color = *p_green_spot; //green value from if statement for crosshair
             p_color++;
             *p_color = (200*x*y/bitmap->width/bitmap->height); //red bottom right
             p_color++;
@@ -125,8 +151,20 @@ draw_test(offscreen_bitmap * bitmap){
         p_row += pitch; //Moves pointer to next row
     }
     intensity++;
+
 }
 
+
+////////////////////////////////
+//Returns dimensions of RECT for a given window
+window_dimensions get_window_dimensions(HWND window){
+    RECT client_rect;
+    GetClientRect(window, &client_rect);
+    window_dimensions dimensions;
+    dimensions.width = client_rect.right - client_rect.left;
+    dimensions.height = client_rect.bottom - client_rect.top;
+    return dimensions;
+};
 
 ////////////////////////////////
 //Takes a pointer to an offscreen_bitmap and resizes it to argument size
@@ -154,17 +192,18 @@ resize_dib_section(offscreen_bitmap * bitmap,int width, int height){
     //Manual memory allocation for bitmap:  
       if(bitmap->p_memory){
         VirtualFree(bitmap->p_memory, 0, MEM_RELEASE);
-        OutputDebugStringA("Offscrren bitmap memory released:");
+        OutputDebugStringA("Offscrren bitmap memory released:\n");
     }
 
     int bitmap_memory_size = bitmap->width*bitmap->height*bitmap->bytes_per_pixel;
     bitmap->p_memory = VirtualAlloc(0, bitmap_memory_size, MEM_COMMIT, PAGE_READWRITE);
 
     if(bitmap->p_memory){
-        OutputDebugStringA("Offscrren bitmap memory reallocated:");
+        OutputDebugStringA("Offscrren bitmap memory reallocated:\n");
     }   
      
 }
+
 ////////////////////////////////
 //Function to update window bitmap size. 
 // Copies a rectangle to another rect, scales if needed. Destination is at the pointer
@@ -181,6 +220,8 @@ update_window(HDC devicecontext, offscreen_bitmap * bitmap ,int x, int y, int wi
     );
 }
 
+
+
 ////////////////////////////////////////////////////////////////
 //WINDOW CALLBACK FUNCTIONS
 
@@ -195,13 +236,14 @@ main_window_callback(
     LPARAM lparam )
     {   
         LRESULT result = 0;
+        kb_y_input = 0;
         switch(message){
             case WM_ACTIVATEAPP: {OutputDebugStringA("WM_ACTIVATEAPP\n");} break;
 
             case WM_SIZE: {
                 window_dimensions dimensions = get_window_dimensions(window);
                 resize_dib_section(p_backbuffer, dimensions.width, dimensions.height);
-                draw_test(p_backbuffer);                
+                draw_test(p_backbuffer, int(1));                
                 OutputDebugStringA("WM_SIZE message\n");            
             } break;
 
@@ -231,14 +273,98 @@ main_window_callback(
         
             } break;
 
+            //Keyboard input. Note lack of break;
+            case WM_SYSKEYDOWN:
+            case WM_SYSKEYUP: 
+            case WM_KEYDOWN:
+            case WM_KEYUP: {
+                UINT32 virtual_key_code = wparam;
+                LPARAM lparam_bits = lparam;
+                bool previous_state = ( lparam_bits & (1 << 30) );      //bit 30 indicates prev state 1 = was pressed
+                bool pressed = ( (lparam_bits & (1 << 31)) == 0 );   //bit 31 indicates transition state 0 = is down
+
+                switch(virtual_key_code){
+                    //arrow keys
+                    case VK_UP:         if(pressed){kb_y_input = -10;} else {kb_y_input = 0;}     break;  
+                    case VK_LEFT:       if(pressed){kb_x_input = -10;} else {kb_x_input = 0;}      break;    
+                    case VK_RIGHT:      if(pressed){kb_x_input = 10;} else {kb_x_input = 0;}      break;    
+                    case VK_DOWN:       if(pressed){kb_y_input = 10;} else {kb_y_input = 0;}      break;  
+
+                    //special keys
+                    case VK_SPACE:       break;     //Space key
+                    case VK_RETURN:      break;     //Enter key
+                    case VK_LSHIFT:      break;     //Left Shift key
+                    case VK_RSHIFT:      break;     //Right Shift key
+                    case VK_LCONTROL:    break;		//Left Ctrl key
+                    case VK_RCONTROL:    break;		//Right Ctrl key
+                    case VK_MENU:        break; 	//Alt key
+                    case VK_LMENU:       break;     //Left Alt key
+                    case VK_RMENU:       break;     //Right Alt key
+                    case VK_TAB:         break;	    //Tab key                    
+                    case VK_CAPITAL:     break;	    //Caps lock key  
+
+                    //characters
+                    case '0':     break;		//0 key
+                    case '1':     break;		//1 key
+                    case '2':     break;		//2 key
+                    case '3':     break;		//3 key
+                    case '4':     break;		//4 key
+                    case '5':     break;		//5 key
+                    case '6':     break;		//6 key
+                    case '7':     break;		//7 key
+                    case '8':     break;		//8 key
+                    case '9':     break;		//9 key
+                    case 'A':     break;		//A key
+                    case 'B':     break;		//B key
+                    case 'C':     break;		//C key
+                    case 'D':     break;		//D key
+                    case 'E':     break;		//E key
+                    case 'F':     break;		//F key
+                    case 'G':     break;		//G key
+                    case 'H':     break;		//H key
+                    case 'I':     break;		//I key
+                    case 'J':     break;		//J key
+                    case 'K':     break;		//K key
+                    case 'L':     break;		//L key
+                    case 'M':     break;		//M key
+                    case 'N':     break;		//N key
+                    case 'O':     break;		//O key
+                    case 'P':     break;		//P key
+                    case 'Q':     break;		//Q key
+                    case 'R':     break;		//R key
+                    case 'S':     break;		//S key
+                    case 'T':     break;		//T key
+                    case 'U':     break;		//U key
+                    case 'V':     break;		//V key
+                    case 'W':     break;		//W key
+                    case 'X':     break;		//X key
+                    case 'Y':     break;		//Y key
+                    case 'Z':     break;		//Z key
+
+                    //mouse buttons
+                    case VK_LBUTTON	:    break;        
+                    case VK_RBUTTON	:    break;        
+                    case VK_CANCEL:      break;      
+                    case VK_MBUTTON:     break;   
+                    case VK_XBUTTON1:    break;        
+                    case VK_XBUTTON2:    break;
+
+                }
+
+
+
+
+            } break;
 
             default: {
-                OutputDebugStringA("default\n");
+                OutputDebugStringA("Callback function default\n");
                 result = DefWindowProc(window, message, wparam, lparam);  //Calling the windows default window procedure for some of the messages DefWindowProc
             } break;
-        }
-        return result;
-    }
+        }        
+    return result;
+}
+
+
 
 /////////////////////////////////////////////////////////////
 //WINMAIN
@@ -280,6 +406,7 @@ int CALLBACK WinMain(
             program_running = true;  
             MSG message;
             BOOL message_bool = 0;
+            load_xinput();      //Loads XInput Get and Set functions manually
 
             //program loop 
             while(program_running)
@@ -296,13 +423,16 @@ int CALLBACK WinMain(
                         if(message.message == WM_QUIT){ program_running = false;}
                         TranslateMessage(&message);                 
                         DispatchMessageA(&message);
-                        OutputDebugStringA("message loop");                        
+                        OutputDebugStringA("message loop\n");                        
                        
                 }
                 
-                //Gamepad and KB inputs
+                //Gamepad input Keyboard input is done through the window Callback function                
+                int test_y_input = kb_y_input; // For draw_test().
+                int test_x_input = kb_x_input;
 
-                //Button BOOL states:
+
+                //Gamepad button BOOL states:
                 bool input_up,input_down,input_left,input_right,input_start,input_back,input_left_shoulder,input_right_shoulder,input_a,input_b,input_x,input_y;
                 input_up=input_down=input_left=input_right=input_start=input_back=input_left_shoulder=input_right_shoulder=input_a=input_b=input_x=input_y=false;
 
@@ -318,7 +448,7 @@ int CALLBACK WinMain(
                     input_error_code = XInputGetState(index, &xinput_state);
                     
                     if(input_error_code == ERROR_SUCCESS){
-                        OutputDebugStringA("Controller input get.");
+                        OutputDebugStringA("Controller input get.\n");
                         WORD * p_buttons = &xinput_state.Gamepad.wButtons;
 
                         input_up= *p_buttons &  XINPUT_GAMEPAD_DPAD_UP;
@@ -346,15 +476,15 @@ int CALLBACK WinMain(
 
                     }
                     else{
-                        OutputDebugStringA("No XInput controller.");
+                        OutputDebugStringA("No XInput controller.\n");
                     }
                     
                 }
                 
 
-
+                
                 //TODO: Test draw should animate background as program runs but doesnt:
-                draw_test(p_backbuffer);
+                draw_test(p_backbuffer, test_y_input, test_x_input);
                 window_dimensions dimensions = get_window_dimensions(window);
                 HDC device_context = GetDC(window);
                 update_window(device_context, p_backbuffer,0,0,dimensions.width,dimensions.height);
@@ -364,7 +494,7 @@ int CALLBACK WinMain(
                     
 
                 //program loop ends
-                OutputDebugStringA("program loop");
+                OutputDebugStringA("program loop\n");
                 Sleep(1);
             }
 
@@ -457,6 +587,23 @@ wButtons bit values:
 
 
 
+
+  // lparam in the callback function
+  
+
+
+  lParam
+
+The repeat count, scan code, extended-key flag, context code, previous key-state flag, and transition-state flag, as shown in the following table.
+
+Bits	Meaning
+0-15	The repeat count for the current message. The value is the number of times the keystroke is autorepeated as a result of the user holding down the key. If the keystroke is held long enough, multiple messages are sent. However, the repeat count is not cumulative.
+16-23	The scan code. The value depends on the OEM.
+24	Indicates whether the key is an extended key, such as the right-hand ALT and CTRL keys that appear on an enhanced 101- or 102-key keyboard. The value is 1 if it is an extended key; otherwise, it is 0.
+25-28	Reserved; do not use.
+29	The context code. The value is 1 if the ALT key is down while the key is pressed; it is 0 if the WM_SYSKEYDOWN message is posted to the active window because no window has the keyboard focus.
+30	The previous key state. The value is 1 if the key is down before the message is sent, or it is 0 if the key is up.
+31	The transition state. The value is always 0 for a WM_SYSKEYDOWN message.
 
 
 
